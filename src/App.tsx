02393827d76e950f41d782cbb462a39357fd4a14
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-11 14:19:54 +0600

    Second commit. We created a file with an interface for the array. We created arrays with objects for the menu and for counting foods and prices. Added icons for dishes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import './App.css'
+import cheeseburger from './assets/cheeseburger.png';
+import fries from './assets/fries.png';
+import cola from './assets/cola.png';
+import tea from './assets/tea.png';
+import coffee from './assets/coffee.png';
+import burger from './assets/burger.png';
+import {MenuItems} from "./types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from "react";
 
 const App = () => {
+  const Menu: MenuItems[] = [
+    {name: 'Burger', price: 80, image: burger},
+    {name: 'CheeseBurger', price: 100, image: cheeseburger},
+    {name: 'Fries', price: 60, image: fries},
+    {name: 'Coffee', price: 70, image: coffee},
+    {name: 'Tea', price: 50, image: tea},
+    {name: 'Cola', price: 65, image: cola},
+  ];
+
+  const [orderItems, setOrderItems] = useState([
+    {name: 'Burger', count: 0},
+    {name: 'CheeseBurger', count: 0},
+    {name: 'Fries', count: 0},
+    {name: 'Coffee', count: 0},
+    {name: 'Tea', count: 0},
+    {name: 'Cola', count: 0},
+  ]);
 
 
   return (
